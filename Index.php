159771da426f8a2a36420adf580cd970d630feb2
Bart <bart_vd_wal@hotmail.com>
2015-11-25 11:08:29 +0100

    map verplaatst en shit

--- Index.php
+++ Index.php
@@ -1,11 +1,16 @@
 <?php
     include_once('header.php');
 ?>
-
-
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
-</body>
-</html>
+
+<div class="content">
+    <h1>Test 123</h1>
+    <h2>Dit is de testtext van vandaag LOREM IPSUM DOLOR SIT AMET</h2>
+</div>
+
+<?php
+    include_once('footer.php');
+?>
