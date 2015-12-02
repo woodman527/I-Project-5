5d8d50b41b4c8e62afad7e5043a0f4f968041ba2
Faalski <larsvanvalen@hotmail.com>
2015-12-02 13:06:23 +0100

    Over Ons
    Over ons + plaatjes + aangepaste css

---
+++ OverOns.php
@@ -0,0 +1,45 @@
+<?php
+    include_once('header.php');
+    include_once('footer.php');
+?>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<head>
+<link href="bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="css/sidebar.css" rel="stylesheet">
+<link href="css/font-awesome.min.css" rel="stylesheet">
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <div class="content">
+    <h1 class="Overons">
+      Over ons
+    </h1>
+    <div class="title">
+      iConcepts
+    </div>
+    <div class="txt">
+      De veilingsite EenmaalAndermaal is gerealiseerd in opdracht van het bedrijf iConcepts. De directeur van iConcepts, de heer Anton Mijnder, heeft besloten een veilingsite te laten ontwikkelen,
+      om zo een groter deel van de Nederlandse bevolking te kunnen aanspreken.
+      Daarnaast wil iConcepts door middel van deze site een bijdrage doen aan de verduurzaamheid van de samenleving.
+    </div>
+    <div class="Image">
+      <img src="Images/AntonMijnder.jpg" alt="AntonMijnder">
+    </div>
+    <div class="title">
+      Routebeschrijving
+    </div>
+    <div class="txt">
+      Hiernaast is een kaart te vinden met daarop de locatie van iConcepts
+    </div>
+    <div class="Image">
+      <img src="Images/Map.png" alt="Kaart">
+    </div>
+
+  </div>
+
+
+</body>
