fea41e7f8755a05fb5652d3f06afe72ffbdb4a22
Bart <bart_vd_wal@hotmail.com>
2015-12-02 13:11:58 +0100

    index.php gerepareerd yaaaay!

--- Index.php
+++ Index.php
@@ -1,15 +1,53 @@
 <?php
     include_once('header.php');
     include_once('footer.php');
-    include('sidebar.php');
+    include_once('sidebar.php');
 ?>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
 
-<body>
-
-    
-</body>
-
+<div class="content">
+    <div class="row">
+        <div class="veilingitem">
+            <h3>Ikea Tafellamp (Zo goed als nieuw)</h3>
+            <img class="media-object" src="Images/voorbeeldlamp.JPG" alt="...">
+            <a class="button" href="voorbeeldvoorwerp.php">Bied Mee</a>
+            <h4>Huidig bod: 22 Euro</h4>
+            <p>Hier komt een testitem te staan inclusief afbeelding en link naar een uitgebreide beschrijving, deze beschrijving
+            moet hoe lang hij ook is, steeds onder de afbeelding blijven staan en niet van het shcerm af lopen. anders gaat alles kapot
+            en moet bart huilen.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="veilingitem">
+            <h3>Ikea Tafellamp (Zo goed als nieuw)</h3>
+            <img class="media-object" src="Images/voorbeeldlamp.JPG" alt="...">
+            <h4>Huidig bod: 22 Euro</h4>
+            <p>Hier komt een testitem te staan inclusief afbeelding en link naar een uitgebreide beschrijving, deze beschrijving
+                moet hoe lang hij ook is, steeds onder de afbeelding blijven staan en niet van het shcerm af lopen. anders gaat alles kapot
+                en moet bart huilen.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="veilingitem">
+            <h3>Ikea Tafellamp (Zo goed als nieuw)</h3>
+            <img class="media-object" src="Images/voorbeeldlamp.JPG" alt="...">
+            <h4>Huidig bod: 22 Euro</h4>
+            <p>Hier komt een testitem te staan inclusief afbeelding en link naar een uitgebreide beschrijving, deze beschrijving
+                moet hoe lang hij ook is, steeds onder de afbeelding blijven staan en niet van het shcerm af lopen. anders gaat alles kapot
+                en moet bart huilen.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="veilingitem">
+            <h3>Ikea Tafellamp (Zo goed als nieuw)</h3>
+            <img class="media-object" src="Images/voorbeeldlamp.JPG" alt="...">
+            <h4>Huidig bod: 22 Euro</h4>
+            <p>Hier komt een testitem te staan inclusief afbeelding en link naar een uitgebreide beschrijving, deze beschrijving
+                moet hoe lang hij ook is, steeds onder de afbeelding blijven staan en niet van het shcerm af lopen. anders gaat alles kapot
+                en moet bart huilen.</p>
+        </div>
+    </div>
+</div>
