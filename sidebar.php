433950f1316b9c0ca1b11eb163b5c58695128979
GedrogeerdeBanaan <ciyan97@live.nl>
2015-12-02 12:23:31 +0100

    Register form
    and shizzle

--- sidebar.php
+++ sidebar.php
@@ -28,21 +28,19 @@
     </div>
     <div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="#">Auto's<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
+        <li><a href="#">Auto's</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Kleding <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Kleding <span class="caret"></span></a>
           <ul class="dropdown-menu forAnimate" role="menu">
-            <li><a href="{{URL::to('createusuario')}}">Crear</a></li>
+            <li><a href="#">Onderbroeken</a></li>
             <li><a href="#">T-shirts</a></li>
             <li><a href="#">Broeken</a></li>
-            <li class="divider"></li>
             <li><a href="#">Schoenen</a></li>
-            <li class="divider"></li>
             <li><a href="#">Sokken</a></li>
           </ul>
         </li>          
-        <li ><a href="#">Fietsen<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-th-list"></span></a></li>        
-        <li ><a href="#">Elektronica<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-tags"></span></a></li>
+        <li ><a href="#">Fietsen</a></li>        
+        <li ><a href="#">Elektronica</a></li>
       </ul>
     </div>
   </div>
